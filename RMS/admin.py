74dba4f3137af7234be404427c194407e7c4c525
CodeKnight <dearjinni44@gmail.com>
2026-01-05 14:54:18 +0545

    Serializer

--- RMS/admin.py
+++ RMS/admin.py
@@ -7,10 +7,46 @@ from .models import *
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
   list_display=['id', 'name']
+
+  
+@admin.register(Food)
+class CategoryAdmin(admin.ModelAdmin):
+  list_display=['id', 'name','description', 'price', 'category']
+  list_filter=['category']
+  search_fields=['name']
   
   
-admin.site.register(Food)
-admin.site.register(Table)
-admin.site.register(Order)
-admin.site.register(OrderItems)
+# class OrderItemInline(admin.StackedInline):
+class OrderItemInline(admin.TabularInline):
+   model = OrderItems
+   autocomplete_fields = ['food']
+
+
+
+@admin.register(Table)
+class CategoryAdmin(admin.ModelAdmin):
+  list_display=['id', 'number', 'capacity', 'available']
+
+
+@admin.register(Order)
+class CategoryAdmin(admin.ModelAdmin):
+  list_display=['id', 'user', 'table', 'total_price', 'status', 'payment_status']
+  list_filter=['status', 'payment_status']
+  search_fields=['user__username' ]
+  inlines = [OrderItemInline]
+  
+  
+  
+@admin.register(OrderItems)
+class CategoryAdmin(admin.ModelAdmin):
+  list_display=['id', 'order', 'food']
+ 
+
+
+  
+  
+# admin.site.register(Food)
+# admin.site.register(Table)
+# admin.site.register(Order)
+# admin.site.register(OrderItems)
 
---
+++ RMS/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from .views import category_list
+
+urlpatterns = [
+    path('/category/', category_list)
+]
